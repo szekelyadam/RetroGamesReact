5c00edca819e876f295cbaf870e72db555b2f9f1
Ádám Székely <szekelyadam@icloud.com>
2017-04-02 12:06:11 +0200

    Basic CRUD is ready

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,22 @@
 import About from './About';
 import Contact from './Contact';
+import Form from './Form';
+import Game from './Game';
+import GameListManager from './GameListManager';
 import Home from './Home';
+import Archive from './Archive';
+import Modal from './Modal';
 import Welcome from './Welcome';
 
 // We export all the components at once
-export { About, Contact, Home, Welcome };
+export {
+  About,
+  Contact,
+  Form,
+  Game,
+  GameListManager,
+  Home,
+  Archive,
+  Modal,
+  Welcome,
+};
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
-import { Home, Welcome, About, Contact } from './components';
+import { AddGameContainer, GamesContainer } from './containers';
+import { Home, Welcome, About, Contact, Archive } from './components';
 
 // Use hashHistory for easier development
 const routes = (
@@ -9,6 +10,10 @@ const routes = (
       <IndexRoute component={Welcome} />
       <Route path="/about" component={About} />
       <Route path="/contact" component={Contact} />
+    </Route>
+    <Route path="/games" component={Archive}>
+      <IndexRoute component={GamesContainer} />
+      <Route path="add" component={AddGameContainer} />
     </Route>
   </Router>
 );
